952e76cf4150e4eec20418345f6fe8181b5716f1
Mariana Santos <maririb51@gmail.com>
2022-06-21 13:11:41 +0100

    Começando a resolver um segundo problema exemplo

---
+++ src/application/Program_2.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//leitura dos dados do produto
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		// impressão provisória
+		
+		System.out.println(product.name + ","+ product.price + ","+ product.quantity);
+		
+		
+		
+		sc.close();
+	}
+
+}
