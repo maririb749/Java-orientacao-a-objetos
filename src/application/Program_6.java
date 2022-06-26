49921f4291c98e667132a5148ac5533f90a65345
Mariana Santos <maririb51@gmail.com>
2022-06-26 13:03:30 +0100

    Membros estáticos-classe Calculator com membros de instância

--- src/application/Program_6.java
+++ src/application/Program_6.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.calculator;
+
 public class Program_6 {
 
 	/*
@@ -11,35 +13,26 @@ public class Program_6 {
 	 * valor. Informar o valor de PI com duas casas decimais.
 	 */
 
-	public static final double PI = 3.14159;
-
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		calculator calc = new calculator();
 
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 
-		double c = circumference(radius);
+		double c = calc.circumference(radius);
 
-		double v = volume(radius);
+		double v = calc.volume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", PI);
+		System.out.printf("PI value: %.2f%n", calc.PI);
 
 		sc.close();
 
 	}
-	
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-	
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius / 3.0;
-	}
-		
 
 }
