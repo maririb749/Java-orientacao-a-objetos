ca5022b639f7eed3dac6a674ff38d1a68f7334e4
Mariana Santos <maririb51@gmail.com>
2022-06-28 12:30:26 +0100

    Sobrecarga

--- src/application/Program_2.java
+++ src/application/Program_2.java
@@ -12,23 +12,21 @@ public class Program_2 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		// leitura dos dados do produto
+		Product p = new Product();
 
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
 
 		System.out.println();
 		System.out.println("Product data: " + product);
 
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
 
 		System.out.println();
