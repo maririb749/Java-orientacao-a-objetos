4da1cb0eadc22653c491fd16992c7fc8c0d89ed3
Mariana Santos <maririb51@gmail.com>
2022-06-28 13:40:40 +0100

    Encapsulamento (manual)

--- src/application/Program_2.java
+++ src/application/Program_2.java
@@ -20,6 +20,11 @@ public class Program_2 {
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
 		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
 
 		System.out.println();
 		System.out.println("Product data: " + product);
